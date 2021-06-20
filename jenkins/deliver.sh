59b962f726b3719e5b1f63fe402382132576b3ba
kingwsi <kingwsi@icloud.com>
2021-06-20 19:26:51 +0800

    deliver.sh

--- jenkins/deliver.sh
+++ jenkins/deliver.sh
@@ -7,16 +7,17 @@ fi
 echo 'copy to apps'
 cp base-admin/target/base-admin-1.0-SNAPSHOT.jar ./apps/admin-server.jar
 
+echo 'stop server'
 # stop server
-sp_pid=`ps -ef | grep admin-server | grep -v grep | awk '{print $2}'`
-if [ -z "$sp_pid" ];
+pid=`ps -ef | grep admin-server | grep -v grep | awk '{print $2}'`
+if [ -z "$pid" ];
 then
   echo "[ not find admin-server pid ]"
 else
-  echo "find result: $sp_pid "
-  kill -9 $sp_pid
+  echo "find result: $pid "
+  kill -9 $pid
 fi
 
 echo 'start application'
 set -x
-java -jar -Dname=admin-server -Duser.timezone=Asia/Shanghai -Xms128M -Xmx256M -XX:MaxNewSize=128M admin-server.jar --spring.profiles.active=test  > admin.log 2>&1 &
+java -jar -Dname=admin-server -Duser.timezone=Asia/Shanghai -Xms128M -Xmx256M -XX:MaxNewSize=128M ./apps/admin-server.jar --spring.profiles.active=test  > ./apps/admin.log 2>&1 &
