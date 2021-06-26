b81a03cf88eacbfbc1b905b68d00bb23d0b5b612
kingwsi <kingwsi@icloud.com>
2021-06-26 21:50:17 +0800

    cors

--- gateway/src/main/java/com/example/gateway/config/GlobalConfiguration.java
+++ gateway/src/main/java/com/example/gateway/config/GlobalConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 /**
  * description: GlobalConfiguration <br>
@@ -22,5 +26,26 @@ public class GlobalConfiguration {
     @Bean
     HttpMessageConverters httpMessageConverters() {
         return new HttpMessageConverters();
+    }
+
+    /**
+     * 配置跨域
+     * @return
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // cookie跨域
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        // 配置前端js允许访问的自定义响应头
+        config.addExposedHeader("Authorization:");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
